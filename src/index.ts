28a2e566f037d6d610aa189099af7922ee2fb9f9
pachet <pachet@burninggarden.com>
2019-09-21 12:58:47 -0400

    Publicly expose different error types

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {default as BaseError}               from 'errors/base';
+export {default as InvalidMessageTypeError} from 'errors/invalid-message-type';
+export {default as InvalidValueError}       from 'errors/invalid-value';
