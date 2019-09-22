be50f4e8d0f2bc87eb47b14e19ea80c7a29f4755
pachet <pachet@burninggarden.com>
2019-09-22 07:48:16 -0400

    Expose error type enum

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as BaseError}               from 'errors/base';
 export {default as InvalidMessageTypeError} from 'errors/invalid-message-type';
 export {default as InvalidValueError}       from 'errors/invalid-value';
+export {default as ErrorType}               from 'enums/error-type';
